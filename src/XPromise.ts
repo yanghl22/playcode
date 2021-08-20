0aab8af854586f36fb8eca571d77f475063dfd87
honglin <honglin.yang.1985@gmail.com>
2021-08-20 14:30:10 +0800

    implement foundementals of XPromise

--- src/XPromise.ts
+++ src/XPromise.ts
@@ -1,59 +1,69 @@
-interface handler {
-  onFulfilled: any;
-  onRejected: any;
-  nextResolve: any;
-  nextReject: any;
+type Resolve = (value: any) => any;
+type Reject = (reason: any) => any;
+type ResolveType = Resolve | undefined;
+type RejectType = Reject | undefined;
+type Executor = (resolve: Resolve, reject: Reject) => void;
+interface Handler {
+  onFulfilled: ResolveType;
+  onRejected: RejectType;
+  nextResolve: ResolveType;
+  nextReject: RejectType;
 }
-
-type handlers = handler[];
-
-class XPromise {
+export default class XPromise {
   status = "pending";
   value = undefined;
   reason = undefined;
-  handlers: handlers = [];
-  constructor(executor: any) {
-    executor(this.resolve.bind(this), this.reject.bind(this));
+  handlers: Handler[] = [];
+  constructor(executor: Executor) {
+    executor(this.resolve, this.reject);
   }
 
-  resolve(value: any) {
+  resolve = (value: any) => {
+    if (value instanceof XPromise) {
+      value.then(this.resolve);
+      return;
+    }
     console.log("XPromise resolved! value:", value);
 
     this.value = value;
     this.status = "fulfilled";
-    this.handlers.forEach(this.handle.bind(this));
-  }
+    this.handlers.forEach(this.handle);
+  };
 
-  reject(reason: any) {
+  reject = (reason: any) => {
     console.log("XPromise rejected! reason:", reason);
     this.reason = reason;
     this.status = "rejected";
-  }
-  handle(cb: handler) {
-    const { onFulfilled, onRejected, nextResolve, nextReject } = cb;
-    if (this.status === "fulfilled") {
-      let nextValue = onFulfilled(this.value);
-      nextResolve(nextValue);
-    }
-  }
-  then(onFulfilled: any, onRejected: any) {
-    return new XPromise((nextResolve: any, nextReject: any) => {
-      this.handlers.push({ onFulfilled, onRejected, nextResolve, nextReject });
-    });
-  }
-}
-const fetchData = (flag: boolean) => {
-  return new XPromise((resolve: any, reject: any) => {
-    setTimeout(() => {
-      if (flag) {
-        resolve(5);
-      } else {
-        reject("error");
-      }
-    }, 1000);
-  });
-};
+  };
 
-fetchData(true).then((value: any) => {
-  console.log("then value:", value);
-}, null);
+  handle = (cb: Handler) => {
+    const { onFulfilled, onRejected, nextResolve, nextReject } = cb;
+
+    if (this.status === "pending") {
+      //   console.log("callback is pushed to handlers!");
+      this.handlers.push(cb);
+    } else if (this.status === "fulfilled") {
+      let nextValue = onFulfilled && onFulfilled(this.value);
+      nextValue && nextResolve && nextResolve(nextValue);
+    } else {
+      let nextReason = onRejected && onRejected(this.reason);
+      nextReject && nextReject(nextReason);
+    }
+  };
+
+  then = (
+    onFulfilled: ResolveType = undefined,
+    onRejected: RejectType = undefined
+  ) => {
+    return new XPromise((nextResolve, nextReject) => {
+      this.handle({
+        onFulfilled,
+        onRejected,
+        nextResolve,
+        nextReject,
+      });
+    });
+  };
+}
+
+
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,26 @@
-const sayHello = (name: string) =>{
-    console.log(`Hello ${name}`)
-}
+import XPromise from "./XPromise";
 
+const fetchData = (flag: boolean) => {
+  return new XPromise((resolve, reject) => {
+    setTimeout(() => {
+      if (flag) {
+        resolve(
+          new XPromise((resolve, reject) => {
+            setTimeout(() => {
+              resolve(3);
+            }, 0);
+          })
+        );
+      } else {
+        reject("error");
+      }
+    }, 0);
+  });
+};
 
-sayHello("honglin")
+fetchData(true).then((value) => {
+  console.log("then value:", value);
+  return value + 1;
+});
+
+console.log("1");
